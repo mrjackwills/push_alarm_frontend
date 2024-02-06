e0d5cd02b9cc9d332480a8cccb7989620a889469
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-02-06 11:56:45 +0000

    fix: alarm status display tweak

--- src/components/AlarmStatus.vue
+++ src/components/AlarmStatus.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-row align='center' justify='center'>
-		<v-col cols='auto' class='text-caption'>
+		<v-col cols='12' class='text-caption' v-if='current_time'>
 
 			<v-row justify='center' class='ma-0 pa-0' v-if='cache'>
 				<v-col cols='auto' class='text-error font-weight-black'>
@@ -8,40 +8,41 @@
 				</v-col>
 			</v-row>
 
-			<v-row justify='space-between' class='ma-0 pa-0' v-if='current_time'>
-				<v-col cols='6' class='ma-0 pa-0'>
-					alarm: <span class='font-weight-bold' v-if='alarm'>{{ hour }}:{{ minute }}</span>
+			<v-row justify='space-between' class='ma-0 pa-0'>
+				<v-col cols='auto' class='ma-0 pa-0'>
+					alarm: <span class='font-weight-bold text-error' v-if='alarm'>{{ hour }}:{{ minute }}</span>
 				</v-col>
-				<v-col cols='6' class='ma-0 pa-0'>
-					time zone: <span class='font-weight-bold'>{{ time_zone }}</span>
+				<v-col cols='auto' class='ma-0 pa-0'>
+					current time: <span class='font-weight-bold text-error'>{{ zpad(current_time.hour) }}:{{
+						zpad(current_time.minute)
+					}}:{{ zpad(current_time.second) }}</span> <span class='font-weight-bold text-primary'>{{ time_zone
+					}}</span>
 				</v-col>
 
-				<v-col cols='6' class='ma-0 pa-0'>
+			</v-row>
+			<v-row justify='space-between' class='ma-0 pa-0'>
+
+				<v-col cols='auto' class='ma-0 pa-0'>
 					backend version: <span class='font-weight-bold'>{{ backend_version }}</span>
 				</v-col>
 
-				<v-col cols='6' class='ma-0 pa-0'>
+				<v-col cols='auto' class='ma-0 pa-0'>
 					frontend version: <span class='font-weight-bold'>{{ env.app_version }}</span>
 				</v-col>
-
-				<v-col cols='6' class='ma-0 pa-0'>
+			</v-row>
+			<v-row justify='space-between' class='ma-0 pa-0'>
+				<v-col cols='auto' class='ma-0 pa-0 text'>
 					uptime app: <span class='font-weight-bold'>{{ uptime_app }}</span>
 				</v-col>
 
-				<v-col cols='6' class='ma-0 pa-0'>
+				<v-col cols='auto' class='ma-0 pa-0'>
 					uptime ws: <span class='font-weight-bold'>{{ uptime_ws }}</span>
 				</v-col>
 
-				<v-col cols='6' class='ma-0 pa-0'>
+				<v-col cols='auto' class='ma-0 pa-0'>
 					uptime: <span class='font-weight-bold'>{{ uptime }}</span>
 				</v-col>
-
-				<v-col cols='6' class='ma-0 pa-0'>
-					current time: <span class='font-weight-bold'>{{ zpad(current_time.hour) }}:{{ zpad(current_time.minute)
-					}}:{{ zpad(current_time.second) }}</span>
-				</v-col>
 			</v-row>
-			<br>
 		</v-col>
 	</v-row>
 </template>
